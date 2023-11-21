81c63fbb99f731db369069549089d6cea317e11e
Gouedar Pierre <pierre.gouedar@etudiant.univ-reims.fr>
2023-11-21 19:36:37 +0100

    Mise à jour de créateur de formulaire du formulaire AnimalType.php

--- src/Form/AnimalType.php
+++ src/Form/AnimalType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Animal;
+use App\Entity\Espece;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,9 +15,17 @@ class AnimalType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomAnimal')
-            ->add('descriptionAnimal')
-            ->add('espece')
+            ->add('nomAnimal', null, ['empty_data' => ''])
+            ->add('descriptionAnimal', null, ['empty_data' => ''])
+            ->add('espece', EntityType::class, [
+                'espece' => Espece::class,
+                'choice_label' => 'libEspece',
+                'required' => true,
+                'query_builder' => function (EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder('e')
+                        ->orderBy('e.libEspece', 'ASC');
+                },
+                ])
         ;
     }
 
