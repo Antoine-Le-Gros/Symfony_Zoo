2183ad786e5b98837afaa615745f22bd4314dbfb
Marc-aurel Lawson <marc-aurel.lawson@etudiant.univ-reims.fr>
2023-11-21 19:56:16 +0100

    Création de l'entité Inscription

---
+++ src/Entity/Inscription.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InscriptionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InscriptionRepository::class)]
+class Inscription
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateInscription = null;
+
+    #[ORM\Column]
+    private ?int $nbPlaceReservees = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateInscription(): ?\DateTimeInterface
+    {
+        return $this->dateInscription;
+    }
+
+    public function setDateInscription(\DateTimeInterface $dateInscription): static
+    {
+        $this->dateInscription = $dateInscription;
+
+        return $this;
+    }
+
+    public function getNbPlaceReservees(): ?int
+    {
+        return $this->nbPlaceReservees;
+    }
+
+    public function setNbPlaceReservees(int $nbPlaceReservees): static
+    {
+        $this->nbPlaceReservees = $nbPlaceReservees;
+
+        return $this;
+    }
+}
