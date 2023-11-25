aea3c580fd409baaff96304a9984a7e42a2fd6c6
Benjamin Haas <benjamin.haas@etudiant.univ-reims.fr>
2023-11-25 16:55:32 +0100

    Ajout de la méthode getAllAnimals

--- src/Repository/EspeceRepository.php
+++ src/Repository/EspeceRepository.php
@@ -21,28 +21,39 @@ class EspeceRepository extends ServiceEntityRepository
         parent::__construct($registry, Espece::class);
     }
 
-//    /**
-//     * @return Espece[] Returns an array of Espece objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('e.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function getAllAnimals(int $idEspece): Espece
+    {
+        $qb = $this->createQueryBuilder('e');
+        $qb->leftJoin('e.animals', 'animals')
+            ->addSelect('animals')
+            ->where('e.id = :id')
+            ->setParameter('id', $idEspece);
 
-//    public function findOneBySomeField($value): ?Espece
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        return $qb->getQuery()->execute()[0];
+    }
+
+    //    /**
+    //     * @return Espece[] Returns an array of Espece objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('e')
+    //            ->andWhere('e.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('e.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Espece
+    //    {
+    //        return $this->createQueryBuilder('e')
+    //            ->andWhere('e.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
