c91a039afd760969724ae20eb19c369b1ba3d598
Benjamin Haas <benjamin.haas@etudiant.univ-reims.fr>
2023-11-25 13:06:41 +0100

    Modification de la méthode index pour ajouter à la vue, les catégories

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieAnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoriesController extends AbstractController
 {
     #[Route('/categories', name: 'app_categories')]
-    public function index(): Response
+    public function index(CategorieAnimalRepository $categoryAnimalRepository): Response
     {
         return $this->render('categories/index.html.twig', [
-            'controller_name' => 'CategoriesController',
+            'categories' => $categoryAnimalRepository->getAllCategories(),
         ]);
     }
 }
