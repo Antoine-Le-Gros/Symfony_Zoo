6a261d002d8fb9440847dce7baaed2bdd2cfa5c8
Benjamin Haas <benjamin.haas@etudiant.univ-reims.fr>
2023-11-25 14:46:19 +0100

    Modification de la méthode index

--- src/Controller/FamiliesController.php
+++ src/Controller/FamiliesController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieAnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FamiliesController extends AbstractController
 {
-    #[Route('/families', name: 'app_families')]
-    public function index(): Response
+    #[Route('/families/{idCategory}', name: 'app_families', requirements: ['idCategory' => '\d+'])]
+    public function index(int $idCategory, CategorieAnimalRepository $categorieAnimalRepository): Response
     {
+        $category = $categorieAnimalRepository->getAllFamilies($idCategory);
+
         return $this->render('families/index.html.twig', [
-            'controller_name' => 'FamiliesController',
+            'families' => $category->getFamilleAnimals(),
+            'categoryName' => $category->getNomCategorie(),
         ]);
     }
 }
