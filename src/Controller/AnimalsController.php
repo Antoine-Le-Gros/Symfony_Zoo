25aba2ee6c2b30c3bcfa85265e89c780e3e6dbbd
Benjamin Haas <benjamin.haas@etudiant.univ-reims.fr>
2023-11-25 16:56:24 +0100

    Modification de l'action index pour envoyer à la vue les animaux et le nom de l'espèce

--- src/Controller/AnimalsController.php
+++ src/Controller/AnimalsController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\EspeceRepository;
+use App\Repository\FamilleAnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnimalsController extends AbstractController
 {
-    #[Route('/animals', name: 'app_animals')]
-    public function index(): Response
+
+
+    #[Route('/animals/{idEspece}', name: 'app_animals', requirements: ['idEspece' => '\d+'])]
+    public function index(int $idEspece, EspeceRepository $especeRepository): Response
     {
+        $espece = $especeRepository->getAllAnimals($idEspece);
+
         return $this->render('animals/index.html.twig', [
-            'controller_name' => 'AnimalsController',
+            'animaux' => $espece->getAnimals(),
+            'especeName' => $espece->getLibEspece(),
         ]);
     }
 }
