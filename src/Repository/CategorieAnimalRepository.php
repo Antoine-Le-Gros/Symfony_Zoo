cf64896a5700c44b08ecd25b8a7e7cbe4c9d1f88
Benjamin Haas <benjamin.haas@etudiant.univ-reims.fr>
2023-11-25 13:06:05 +0100

    creation de la requête avec la jointure externe

--- src/Repository/CategorieAnimalRepository.php
+++ src/Repository/CategorieAnimalRepository.php
@@ -21,28 +21,43 @@ class CategorieAnimalRepository extends ServiceEntityRepository
         parent::__construct($registry, CategorieAnimal::class);
     }
 
-//    /**
-//     * @return CategorieAnimal[] Returns an array of CategorieAnimal objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * Listage des catégories avec leurs familles.
+     *
+     * @return CategorieAnimal[]
+     */
+    public function getAllCategories(): array
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb->leftJoin('c.familleAnimals', 'familles')
+            ->addSelect('familles')
+            ->groupBy('c.id');
 
-//    public function findOneBySomeField($value): ?CategorieAnimal
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        return $qb->getQuery()->execute();
+    }
+
+    //    /**
+    //     * @return CategorieAnimal[] Returns an array of CategorieAnimal objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('c.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?CategorieAnimal
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
