c3455c2459eabdea1ce176cd794fefdcb9287b04
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-11-26 20:56:41 +0100

    Changement de la méthode load de EnclosFixtures.php
    Création de plusieurs enclos

--- src/DataFixtures/EnclosFixtures.php
+++ src/DataFixtures/EnclosFixtures.php
@@ -2,17 +2,14 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\EnclosFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class EnclosFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        EnclosFactory::createMany(5);
     }
 }
