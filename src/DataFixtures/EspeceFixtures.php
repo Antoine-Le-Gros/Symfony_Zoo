6d62895a4c5ac06ff4912689b9be88de6479eca0
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-11-26 20:58:37 +0100

    Changement de la méthode load de EspeceFixtures.php
    Récupération des données du fichier NomEspece.json et création des entités

--- src/DataFixtures/EspeceFixtures.php
+++ src/DataFixtures/EspeceFixtures.php
@@ -2,16 +2,18 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\EspeceFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class EspeceFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $file = file_get_contents(__DIR__.'/data/NomEspece.json');
+        $file_j = json_decode($file, true);
+        EspeceFactory::createSequence($file_j);
+    }
 
         $manager->flush();
     }
