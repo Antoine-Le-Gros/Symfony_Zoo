cb9d8e5953d77122d2bab20f76e5a4c3f6bfbb3a
Benjamin Haas <benjamin.haas@etudiant.univ-reims.fr>
2023-11-26 14:46:36 +0100

    Modification de l'action index pour récupérer et utiliser le paramètre search

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use App\Repository\CategorieAnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoriesController extends AbstractController
 {
     #[Route('/categories', name: 'app_categories')]
-    public function index(CategorieAnimalRepository $categoryAnimalRepository): Response
+    public function index(Request $request, CategorieAnimalRepository $categoryAnimalRepository): Response
     {
+        $search = $request->query->get('search', '');
+        $categories = $categoryAnimalRepository->getAllCategories($search);
         return $this->render('categories/index.html.twig', [
-            'categories' => $categoryAnimalRepository->getAllCategories(),
+            'categories' => $categories,
+            'search' => $search
         ]);
     }
 }
