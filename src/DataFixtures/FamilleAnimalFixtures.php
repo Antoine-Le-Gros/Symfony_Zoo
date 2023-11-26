64a5b14f82457b430c59d89796944d9f24997883
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-11-26 20:59:56 +0100

    Changement de la méthode load de FamilleAnimalFixtures.php
    Récupération des données du fichier Famille.json et création des entités

--- src/DataFixtures/FamilleAnimalFixtures.php
+++ src/DataFixtures/FamilleAnimalFixtures.php
@@ -2,16 +2,18 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\FamilleAnimalFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class FamilleAnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $file = file_get_contents(__DIR__.'/data/Famille.json');
+        $file_j = json_decode($file, true);
+        FamilleAnimalFactory::createSequence($file_j);
+    }
 
         $manager->flush();
     }
