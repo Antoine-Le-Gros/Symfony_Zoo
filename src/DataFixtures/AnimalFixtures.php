8807126f8696490cfa9228677e96092889242a89
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-11-27 10:17:25 +0100

    Ajout de la prise en compte des parents des animaux

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Factory\AnimalFactory;
+use App\Factory\EnclosFactory;
+use App\Factory\EspeceFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -11,7 +13,27 @@ class AnimalFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        AnimalFactory::createMany(20);
+        AnimalFactory::createMany(20, function () {
+            if (AnimalFactory::faker()->boolean(30)) {
+                $espece = EspeceFactory::random();
+                $enclos = EnclosFactory::random();
+
+                return [
+                    'espece' => $espece,
+                    'enclos' => $enclos,
+                    'parent1' => AnimalFactory::new([
+                        'espece' => $espece,
+                        'enclos' => $enclos,
+                    ]),
+                    'parent2' => AnimalFactory::new([
+                        'espece' => $espece,
+                        'enclos' => $enclos,
+                    ]),
+                ];
+            }
+
+            return [];
+        });
     }
 
     public function getDependencies(): array
