bea9caf8d214b2cff7dfa75a9949734b70f79e4c
Gabriel Titeux <gabriel.titeux@etudiant.univ-reims.fr>
2023-12-01 11:33:48 +0100

    Ajout du test formDeleteAnimalDenied dans DeleteCest.php d'animal test OK

--- tests/Controller/Animal/DeleteCest.php
+++ tests/Controller/Animal/DeleteCest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller\Animal;
 
+use App\Entity\Animal;
 use App\Factory\AnimalFactory;
 use App\Factory\CategorieAnimalFactory;
 use App\Factory\EnclosFactory;
@@ -9,25 +10,39 @@ use App\Factory\EspeceFactory;
 use App\Factory\FamilleAnimalFactory;
 use App\Factory\RegimeFactory;
 use App\Tests\Support\ControllerTester;
+use Zenstruck\Foundry\Proxy;
 
 class DeleteCest
 {
     public function formDeleteAnimal(ControllerTester $I): void
     {
-        RegimeFactory::createOne();
-        CategorieAnimalFactory::createOne();
-        FamilleAnimalFactory::createOne();
-        EnclosFactory::createOne();
-        EspeceFactory::createOne();
-        $animal = AnimalFactory::createOne(['nomAnimal' => 'Pierre']);
+        $animal = $this->generateAnimalDB();
 
         $I->amOnPage("/animal/{$animal->getId()}/delete");
         $I->seeInTitle("Suppression de {$animal->getNomAnimal()}");
         $I->see("Suppression de {$animal->getNomAnimal()}", 'h1');
     }
 
-    // tests
-    public function tryToTest(ControllerTester $I)
+    public function formDeleteAnimalDenied(ControllerTester $I): void
     {
+        $animal = $this->generateAnimalDB();
+
+        $I->amOnPage("/animal/{$animal->getId()}/delete");
+        $I->click('#form_cancel');
+
+        $I->seeInCurrentUrl("/animal?id={$animal->getId()}");
+        // Changer la ligne du dessus pour celle en dessous pour une fois l'implémentation de la route faite
+        // $I->seeCurrentRouteIs('app_animal', ['id' => $animal->getId()]);
+    }
+
+    public function generateAnimalDB(): Proxy|Animal
+    {
+        RegimeFactory::createOne();
+        CategorieAnimalFactory::createOne();
+        FamilleAnimalFactory::createOne();
+        EnclosFactory::createOne();
+        EspeceFactory::createOne();
+
+        return AnimalFactory::createOne();
     }
 }
