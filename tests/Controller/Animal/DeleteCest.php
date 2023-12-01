6f32f488c8ef58f163a67f99122048c0138d8426
Gabriel Titeux <gabriel.titeux@etudiant.univ-reims.fr>
2023-12-01 11:18:48 +0100

    Ajout du test formDeleteAnimal dans DeleteCest.php d'animal test OK

--- tests/Controller/Animal/DeleteCest.php
+++ tests/Controller/Animal/DeleteCest.php
@@ -2,12 +2,28 @@
 
 namespace App\Tests\Controller\Animal;
 
+use App\Factory\AnimalFactory;
+use App\Factory\CategorieAnimalFactory;
+use App\Factory\EnclosFactory;
+use App\Factory\EspeceFactory;
+use App\Factory\FamilleAnimalFactory;
+use App\Factory\RegimeFactory;
 use App\Tests\Support\ControllerTester;
 
 class DeleteCest
 {
-    public function _before(ControllerTester $I)
+    public function formDeleteAnimal(ControllerTester $I): void
     {
+        RegimeFactory::createOne();
+        CategorieAnimalFactory::createOne();
+        FamilleAnimalFactory::createOne();
+        EnclosFactory::createOne();
+        EspeceFactory::createOne();
+        $animal = AnimalFactory::createOne(['nomAnimal' => 'Pierre']);
+
+        $I->amOnPage("/animal/{$animal->getId()}/delete");
+        $I->seeInTitle("Suppression de {$animal->getNomAnimal()}");
+        $I->see("Suppression de {$animal->getNomAnimal()}", 'h1');
     }
 
     // tests
