5c9f5d059dd131d440ef9bf685fb637e5ef54bc5
Gabriel Titeux <gabriel.titeux@etudiant.univ-reims.fr>
2023-12-03 21:55:43 +0100

    Ajout du test formUpdateAnimal dans UpdateCest.php Test OK

--- tests/Controller/Animal/UpdateCest.php
+++ tests/Controller/Animal/UpdateCest.php
@@ -2,16 +2,36 @@
 
 namespace App\Tests\Controller\Animal;
 
+use App\Factory\AnimalFactory;
+use App\Factory\CategorieAnimalFactory;
+use App\Factory\EnclosFactory;
+use App\Factory\EspeceFactory;
+use App\Factory\FamilleAnimalFactory;
+use App\Factory\RegimeFactory;
 use App\Tests\Support\ControllerTester;
 
 class UpdateCest
 {
-    public function _before(ControllerTester $I)
+    public function formUpdateAnimal(ControllerTester $I): void
     {
-    }
+        RegimeFactory::createOne();
+        CategorieAnimalFactory::createOne();
+        FamilleAnimalFactory::createOne();
+        $enclos = EnclosFactory::createOne(['nomEnclos' => 'Le cirque']);
+        $espece = EspeceFactory::createOne(['libEspece' => 'stone']);
+        AnimalFactory::createOne([
+            'nomAnimal' => 'Pierre',
+            'descriptionAnimal' => 'Pierre est un cailloux',
+            'espece' => $espece,
+            'enclos' => $enclos,
+        ]);
 
-    // tests
-    public function tryToTest(ControllerTester $I)
-    {
+        $I->amOnRoute('app_animal_update', ['id' => 1]);
+        $I->seeInTitle('Édition de Pierre');
+        $I->see('Édition de Pierre', 'h1');
+        $I->seeInField('input[name="animal[nomAnimal]"]', 'Pierre');
+        $I->seeInField('input[name="animal[descriptionAnimal]"]', 'Pierre est un cailloux');
+        $I->seeOptionIsSelected('select[name="animal[espece]"]', 'stone');
+        $I->seeOptionIsSelected('select[name="animal[enclos]"]', 'Le cirque');
     }
 }
