df5da42a9563a0fc966c2f5de6064ed081238c10
Marc-aurel Lawson <marc-aurel.lawson@etudiant.univ-reims.fr>
2023-12-03 16:16:58 +0100

    Ajout du test listOfFamilyAccordingCategory

--- tests/Controller/FamilleAnimal/IndexCest.php
+++ tests/Controller/FamilleAnimal/IndexCest.php
@@ -108,4 +108,50 @@ class IndexCest
         $I->seeResponseCodeIs(200);
         $I->assertEquals(['cebidé description', 'cervidé description'], $I->grabMultiple('.famillesAnimal li'));
     }
+
+    public function listOfFamilyAccordingCategory(ControllerTester $I): void
+    {
+        $category1 = CategorieAnimalFactory::createOne(['nom_categorie' => 'mammifère',
+            'descriptionCategorie' => 'description']);
+        $category2 = CategorieAnimalFactory::createOne(['nom_categorie' => 'oiseau',
+            'descriptionCategorie' => 'description']);
+
+        FamilleAnimalFactory::createSequence(
+            [
+                ['nomFamille' => 'homnidé',
+                    'descriptionFamille' => 'description',
+                    'categorie' => $category1,
+                ],
+
+                ['nomFamille' => 'bovidé',
+                    'descriptionFamille' => 'description',
+                    'categorie' => $category2,
+                ],
+
+                ['nomFamille' => 'félidé',
+                    'descriptionFamille' => 'description',
+                    'categorie' => $category1,
+                ],
+
+                ['nomFamille' => 'cervidé',
+                    'descriptionFamille' => 'description',
+                    'categorie' => $category2,
+                ],
+
+                [
+                    'nomFamille' => 'cebidé',
+                    'descriptionFamille' => 'description',
+                    'categorie' => $category1,
+                ],
+            ]
+        );
+        $I->amOnPage('/categories');
+        $I->click('mammifère description');
+        $I->seeResponseCodeIs(200);
+        $I->seeCurrentRouteIs('app_families');
+        $I->amOnPage('/families/1');
+        $I->seeInTitle('Liste des familles appartenant à la catégorie mammifère');
+        $I->see('Liste des familles appartenant à la catégorie mammifère', 'h1');
+        $I->seeNumberOfElements('.famillesAnimal li', 3);
+    }
 }
