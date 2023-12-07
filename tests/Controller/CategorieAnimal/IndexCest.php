149d009fb3917a529c77b7a4c60d9a5213d11aad
Gabriel Titeux <gabriel.titeux@etudiant.univ-reims.fr>
2023-12-07 15:20:20 +0100

    refactorisation des tests index de categories

--- tests/Controller/CategorieAnimal/IndexCest.php
+++ tests/Controller/CategorieAnimal/IndexCest.php
@@ -10,8 +10,10 @@ class IndexCest
     public function defaultNumberOfCategorieAnimalIs10(ControllerTester $I): void
     {
         CategorieAnimalFactory::createMany(10);
+
         $I->amOnPage('/categories');
         $I->seeResponseCodeIs(200);
+
         $I->seeInTitle('Liste des catégories présentes au sein du zoo');
         $I->see('Liste des catégories présentes au sein du zoo', 'h1');
         $I->seeNumberOfElements('.categoriesAnimal>li>a[href]', 10);
@@ -19,55 +21,80 @@ class IndexCest
 
     public function clickOnFirstElementOfCategoryList(ControllerTester $I): void
     {
-        CategorieAnimalFactory::createOne(['nom_categorie' => 'reptile',
-                                                  'descriptionCategorie' => 'description',
-                                            ]);
+        CategorieAnimalFactory::createOne([
+            'nom_categorie' => 'reptile',
+            'descriptionCategorie' => 'description',
+        ]);
+
         $I->amOnPage('/categories');
+
         $I->click('reptile description');
         $I->seeResponseCodeIs(200);
-        $I->seeCurrentRouteIs('app_families');
+
+        $I->seeCurrentUrlEquals('/families/1');
     }
 
     public function isCategoryListSorted(ControllerTester $I): void
     {
         CategorieAnimalFactory::createSequence(
             [
-                ['nom_categorie' => 'amphibien',
-                    'descriptionCategorie' => 'description'],
-                ['nom_categorie' => 'reptile',
-                    'descriptionCategorie' => 'description'],
-                ['nom_categorie' => 'mammifère',
-                    'descriptionCategorie' => 'description'],
-                ['nom_categorie' => 'oiseau',
-                    'descriptionCategorie' => 'description'],
+                [
+                    'nom_categorie' => 'amphibien',
+                    'descriptionCategorie' => 'description',
+                ],
+                [
+                    'nom_categorie' => 'reptile',
+                    'descriptionCategorie' => 'description',
+                ],
+                [
+                    'nom_categorie' => 'mammifère',
+                    'descriptionCategorie' => 'description',
+                ],
+                [
+                    'nom_categorie' => 'oiseau',
+                    'descriptionCategorie' => 'description',
+                ],
             ]
         );
+
         $I->amOnPage('/categories');
+
         $I->assertEquals(
             [
-            'amphibien description',
-            'mammifère description',
-            'oiseau description',
-            'reptile description',
+                'amphibien description',
+                'mammifère description',
+                'oiseau description',
+                'reptile description',
             ],
-            $I->grabMultiple('.categoriesAnimal li>a'));
+            $I->grabMultiple('.categoriesAnimal li>a')
+        );
     }
 
     public function testSearchCategory(ControllerTester $I): void
     {
         CategorieAnimalFactory::createSequence(
             [
-                ['nom_categorie' => 'amphibien',
-                    'descriptionCategorie' => 'description'],
-                ['nom_categorie' => 'reptile',
-                    'descriptionCategorie' => 'description'],
-                ['nom_categorie' => 'mammifère',
-                    'descriptionCategorie' => 'description'],
-                ['nom_categorie' => 'oiseau',
-                    'descriptionCategorie' => 'description'],
+                [
+                    'nom_categorie' => 'amphibien',
+                    'descriptionCategorie' => 'description',
+                ],
+                [
+                    'nom_categorie' => 'reptile',
+                    'descriptionCategorie' => 'description',
+                ],
+                [
+                    'nom_categorie' => 'mammifère',
+                    'descriptionCategorie' => 'description',
+                ],
+                [
+                    'nom_categorie' => 'oiseau',
+                    'descriptionCategorie' => 'description',
+                ],
             ]
         );
+
         $I->amOnPage('/categories?search=am');
+
         $I->assertEquals(
             [
                 'amphibien description',
