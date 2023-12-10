c7f5ad79251b8fcb79f52f6e9fa46aae02f974b1
Marc-aurel Lawson <marc-aurel.lawson@etudiant.univ-reims.fr>
2023-12-10 14:35:17 +0100

    Création du formulaire récupérant les données d'un évènement pour son édition

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -3,21 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\Evenement;
+use App\Form\EvenementType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EvenementController extends AbstractController
 {
-    #[Route('/evenement/{id}/update', requirements: ['id' => '\id'])]
+    #[Route('/evenement/{id}/update', requirements: ['id' => '\d+'])]
     public function update(Evenement $event): Response
     {
+        $form = $this->createForm(EvenementType::class, $event);
+
         return $this->render('evenement/update.html.twig', [
             'event' => $event,
+            'form' => $form,
         ]);
     }
+
     #[Route('/evenement/create')]
-    public function create():Response {
+    public function create(): Response
+    {
         return $this->render('evenement/create.html.twig');
     }
 }
