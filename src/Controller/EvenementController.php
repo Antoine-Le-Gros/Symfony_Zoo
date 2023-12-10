70f1267c6d9317b59ff4c1756d3fef80f80f996b
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-12-10 13:13:47 +0100

    Modification de la méthode index dans le controller EvenementController.php
    Permet maintenant d'afficher les évènements pour un enclos donné

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Evenement;
+use App\Repository\EnclosRepository;
+use App\Repository\EvenementRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EvenementController extends AbstractController
 {
-    #[Route('/evenement', name: 'app_evenement')]
-    public function index(): Response
+    #[Route('/evenement/{idEnclos}', name: 'app_evenement', requirements: ['idEnclos' => '\d+'])]
+    public function index(int $idEnclos, EnclosRepository $enclosRepository, Request $request): Response
     {
+        $search = $request->query->get('search', '');
+
         return $this->render('evenement/index.html.twig', [
-            'controller_name' => 'EvenementController',
+            'events' => $enclosRepository->getAllEvents($idEnclos, $search),
+            'EnclosName' => $enclosRepository->find($idEnclos)?->getNomEnclos(),
+            'enclos' => true,
+            'search' => $search,
         ]);
     }
 }
