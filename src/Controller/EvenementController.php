16152c7378b96e0230dfe796154247f18019b9ee
Marc-aurel Lawson <marc-aurel.lawson@etudiant.univ-reims.fr>
2023-12-10 10:50:55 +0100

    Ajout de l'action update pour l'édition d'un évènement ainsi que sa vue associée

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Evenement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EvenementController extends AbstractController
 {
-
+    #[Route('/evenement/{id}/update', requirements: ['id' => '\id'])]
+    public function update(Evenement $event): Response
+    {
+        return $this->render('evenement/update.html.twig', [
+            'event' => $event,
+        ]);
+    }
 }
