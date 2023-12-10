fd92b72de3e62ed98220741e416cc334023753b2
Marc-aurel Lawson <marc-aurel.lawson@etudiant.univ-reims.fr>
2023-12-10 17:28:24 +0100

    Persistance d'un nouvel évènement Redirection sur la route app_animal (temporaire à modifier sur show, de même que pour l'édition)

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -6,8 +6,8 @@ use App\Entity\Evenement;
 use App\Form\EvenementType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EvenementController extends AbstractController
@@ -20,7 +20,7 @@ class EvenementController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('');
+            return $this->redirectToRoute('app_animal');
         }
 
         return $this->render('evenement/update.html.twig', [
@@ -30,8 +30,20 @@ class EvenementController extends AbstractController
     }
 
     #[Route('/evenement/create')]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('evenement/create.html.twig');
+        $event = new Evenement();
+        $form = $this->createForm(EvenementType::class, $event);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_animal');
+        }
+
+        return $this->render('evenement/create.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
