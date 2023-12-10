100ee517d782f43aad04ae07d1bb500f6b79c880
Marc-aurel Lawson <marc-aurel.lawson@etudiant.univ-reims.fr>
2023-12-10 16:51:19 +0100

    Persistance des modifications d'un

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -4,16 +4,24 @@ namespace App\Controller;
 
 use App\Entity\Evenement;
 use App\Form\EvenementType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EvenementController extends AbstractController
 {
     #[Route('/evenement/{id}/update', requirements: ['id' => '\d+'])]
-    public function update(Evenement $event): Response
+    public function update(Evenement $event, Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(EvenementType::class, $event);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('');
+        }
 
         return $this->render('evenement/update.html.twig', [
             'event' => $event,
