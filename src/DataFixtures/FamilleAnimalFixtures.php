f7a6d0ee363502b1a3f6c6cb73f4f7349cf7f677
Benjamin Haas <benjamin.haas@etudiant.univ-reims.fr>
2023-12-11 13:49:21 +0100

    Ajout des catégories pour les familles

--- src/DataFixtures/FamilleAnimalFixtures.php
+++ src/DataFixtures/FamilleAnimalFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategorieAnimalFactory;
 use App\Factory\FamilleAnimalFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -13,6 +14,12 @@ class FamilleAnimalFixtures extends Fixture implements DependentFixtureInterface
     {
         $file = file_get_contents(__DIR__.'/data/Famille.json');
         $file_j = json_decode($file, true);
+        $cat = CategorieAnimalFactory::all();
+        $catFamilly = [$cat[0], $cat[0], $cat[0], $cat[0], $cat[0], $cat[0], $cat[0], $cat[0], $cat[1], $cat[2], $cat[3], $cat[0], $cat[0], $cat[0],
+            $cat[1], $cat[1], $cat[0], $cat[0], $cat[1], $cat[2], $cat[2]];
+        for ($i = 0; $i < count($file_j); ++$i) {
+            $file_j[$i]['categorie'] = $catFamilly[$i];
+        }
         FamilleAnimalFactory::createSequence($file_j);
     }
 
