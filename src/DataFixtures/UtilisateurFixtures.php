6ebbafdad1a0f60ac2a7c54835318f7b32fc70aa
Marc-aurel Lawson <marc-aurel.lawson@etudiant.univ-reims.fr>
2023-12-11 20:41:06 +0100

    chargement des utilisateurs et de leurs rôles

--- src/DataFixtures/UtilisateurFixtures.php
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\UtilisateurFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 ;
@@ -10,9 +11,21 @@ class UtilisateurFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        UtilisateurFactory::createMany(20,function (){
+            $bool=UtilisateurFactory::faker()->boolean(15);
+            return [
+                'dateEmbauche'=>$bool ? UtilisateurFactory::faker()->datetime() : null,
+                'dureeContrat'=>$bool ? UtilisateurFactory::faker()->numberBetween(1,365): null,
+                'roles'=>$bool ? ['ROLE_PERSONNEL'] :['ROLE_UTULISATEUR'],
+            ];
 
-        $manager->flush();
+        });
+
+        UtilisateurFactory::createOne([
+            'pnomUser'=>'Pierre',
+            'nomUser'=>'Gouedar',
+            'roles'=>['ROLE_ADMIN']
+        ]);
+
     }
 }
