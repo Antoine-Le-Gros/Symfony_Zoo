710ef6b15b472ebe665f29a2ee0561b353ea6da2
Gabriel Titeux <gabriel.titeux@etudiant.univ-reims.fr>
2023-12-17 11:15:15 +0100

    Rebase fix

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -40,6 +40,7 @@ class AnimalController extends AbstractController
     #[Route('/animal/{id}/update', requirements: ['id' => '\d+'])]
     public function update(EntityManagerInterface $entityManager, Animal $animal, Request $request): RedirectResponse|Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $form = $this->createForm(AnimalType::class, $animal);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -66,6 +67,7 @@ class AnimalController extends AbstractController
     #[Route('/animal/create')]
     public function create(EntityManagerInterface $entityManager, Request $request): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $animal = new Animal();
         $form = $this->createForm(AnimalType::class, $animal);
         $form->handleRequest($request);
@@ -92,6 +94,7 @@ class AnimalController extends AbstractController
         EntityManagerInterface $entityManager,
         Request $request): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $form = $this->createFormBuilder()
             ->add('delete', SubmitType::class)
             ->add('cancel', SubmitType::class)
