7f4ad6e50beeee10cd801cef7c97565af4127715
Gabriel Titeux <gabriel.titeux@etudiant.univ-reims.fr>
2024-01-09 09:35:36 +0100

    Ajout restriction crud event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -75,6 +75,7 @@ class EventController extends AbstractController
         EntityManagerInterface $entityManager,
         Request $request): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $form = $this->createFormBuilder()
             ->add('delete', SubmitType::class)
             ->add('cancel', SubmitType::class)
@@ -102,6 +103,7 @@ class EventController extends AbstractController
     #[Route('/event/{id}/update', requirements: ['id' => '\d+'])]
     public function update(Event $event, Request $request, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -119,6 +121,7 @@ class EventController extends AbstractController
     #[Route('/event/create')]
     public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
