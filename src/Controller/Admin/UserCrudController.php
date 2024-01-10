b559ecb47ce5ba3a4e48625466e3fa04ac6538dc
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2024-01-10 16:12:21 +0100

    Ajout du crud d'user
    non fonctionnel

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserCrudController extends AbstractCrudController
+{
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            EmailField::new('email'),
+            TextField::new('password')
+                ->onlyOnForms()
+                ->setFormType(PasswordType::class)
+                ->setFormTypeOptions([
+                    'required' => false,
+                    'empty_data' => '',
+                    'attr' => ['autocomplete' => 'new-password'],
+                ]),
+            TextField::new('firstname'),
+            TextField::new('lastname'),
+            ArrayField::new('roles')
+                ->formatValue(function (array $roles, User $user) {
+                    $symbols = '';
+                    if (in_array('ROLE_ADMIN', $user->getRoles())) {
+                        $symbols .= '<span class="material-symbols-outlined">manage_accounts</span>';
+                    }
+                    if (in_array('ROLE_EMPLOYEE', $user->getRoles())) {
+                        $symbols .= '<span class="material-symbols-outlined">badge</span>';
+                    }
+                    if (in_array('ROLE_USER', $user->getRoles())) {
+                        $symbols .= '<span class="material-symbols-outlined">person</span>';
+                    }
+
+                    return $symbols;
+                }),
+            DateTimeField::new('dateOfBirth')
+            ->formatValue(function (?\DateTime $dateOfBirth) {
+                return $dateOfBirth->format('Y-m-d H:i') ?: 'Aucune';
+            }),
+            DateTimeField::new('hiringDate')
+                ->formatValue(function (?\DateTime $hiringDate) {
+                    return $hiringDate->format('Y-m-d H:i') ?: 'Aucune';
+                }),
+            IntegerField::new('contractDuration'),
+            DateTimeField::new('dateVisitor')
+                ->formatValue(function (?\DateTime $dateVisitor) {
+                    return $dateVisitor->format('Y-m-d H:i') ?: 'Aucune';
+                }),
+        ];
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPassword($entityInstance);
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPassword($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function setUserPassword($entityInstance): void
+    {
+        $password = $this->getContext()->getRequest()->get('User')['password'];
+        if (!empty($password)) {
+            $entityInstance->setPassword($this->userPasswordHasher->hashPassword($entityInstance, $entityInstance->getPassword()));
+        }
+    }
+}
