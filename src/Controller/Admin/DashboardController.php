0d0ee4830ec07f881ae13a6435e576838eb63053
Benjamin Haas <benjamin.haas@etudiant.univ-reims.fr>
2024-01-11 10:09:26 +0100

    Modification du dashboardController pour ajouter les liens vers les crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Animal;
+use App\Entity\AnimalCategory;
+use App\Entity\AnimalDiet;
+use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -25,6 +29,9 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Régimes', 'fas fa-list', AnimalDiet::class);
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-list', AnimalCategory::class);
+        yield MenuItem::linkToCrud('Animaux', 'fas fa-list', Animal::class);
         // yield MenuItem::linkToCrud('ajouter pour chaque crud', 'fas fa-list', EntityClass::class);
     }
 }
